4367e29f02fefdee3a570b133ac07c9b4d663897
heyyakash <akashsharma2002@gmail.com>
2021-10-30 19:48:58 +0530

    Notes Web App ver1

--- frontend.js
+++ frontend.js
@@ -1,10 +1,20 @@
 const addBtn = document.getElementById("addBtn");
 const colors = document.getElementById("colors");
 const Form = document.getElementById("form");
+const FormContainer = document.getElementById("form-box");
 const FormClose = document.getElementById("form-close");
 const colorBtns =document.querySelectorAll(".add-btn");
+const changeBtn = document.getElementById("change-button");
 addBtn.addEventListener("click", function(){
     colors.classList.toggle("add-btn-show");
+    if(changeBtn.classList[0]=="ri-add-circle-fill"){
+        changeBtn.classList.remove("ri-add-circle-fill");
+        changeBtn.classList.add("ri-close-circle-fill");
+    }
+    else{
+        changeBtn.classList.remove("ri-close-circle-fill");
+        changeBtn.classList.add("ri-add-circle-fill");
+    }
 })
 // ================== ADD ==================
 // === colorbtns ==
@@ -22,6 +32,7 @@ function showFormBox(colorId){
     let formColor = document.getElementById("form-color");
     Form.classList.remove("form-display-none");
     formColor.style.backgroundColor = colorId;
+    FormContainer.style.backgroundColor =colorId;
 }
 
 
